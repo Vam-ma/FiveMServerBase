15113e6fa1098ca4bbdbeb383d20049391ebf319
Vamma <58887108+Vam-ma@users.noreply.github.com>
2023-08-12 19:16:36 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Vamma'
+description 'Player effects, car destruction & repair & a lot more'
+version '1.2.0'
+
+data_file 'HANDLING_FILE' 'handling.meta'
+
+files {
+    'handling.meta'
+}
+
+client_script 'FiveMServerBase_Client.dll'
+server_script 'FiveMServerBase_Server.dll'
